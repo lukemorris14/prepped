a319b9b60f5836b501b66422cead4a15f7492088
lukemorris14 <luke_morris14@hotmail.com>
2020-09-20 20:13:16 -0700

    adding int for git

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+const knex = require('./knex/knex.js');
+const User = require('./app/models/user.js');
+
+app.get('/', (req, res) => {
+  res.status(200).send('Prepped Server is Running!');
+});
+
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Access-Control-Allow-Headers',
+  );
+  next();
+});
+
+// Routes
+app.post('/signup', User.signup);
+app.post('/signin', User.signin)
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}.`);
+});
---
+++ server/knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      port: '5432',
+      password: 'wierd1',
+      database: 'prepped',
+      charset: 'utf8',
+    },
+    migrations: {
+      directory: __dirname + '/knex/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/knex/seeds',
+    },
+  },
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'prepped',
+      user: 'postgres',
+      password: 'wierd1',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'prepped',
+      user: 'postgres',
+      password: 'wierd1',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
